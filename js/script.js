5236ac26560d6d36abd997c9f7b8c8b457d300d4
Sebastián <arevalosebastian081@gmail.com>
2022-03-07 18:21:31 -0500

    postJuegos

--- js/script.js
+++ js/script.js
@@ -8,5 +8,29 @@ const getAll = async() => {
     }
 };
 
+let guardar = document.querySelector('#btnGuardar');
+
+guardar.addEventListener('click', (e) => {
+    e.preventDefault();
+    try {
+        fetch("https://server-games-app.herokuapp.com/games", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                name: document.querySelector('#name').value,
+                price: document.querySelector('#price').value,
+                description: document.querySelector('#description').value,
+                genre: document.querySelector('#genre'.value),
+                cover: document.querySelector('#imagen').value,
+                platform: document.querySelector('#platform').value
+            }),
+          });
+          alert('juego guardado')
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 
