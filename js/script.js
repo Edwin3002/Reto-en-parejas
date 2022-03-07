5642a7a664899d283d15f3b1b5faee6b0eb1cdb4
Edwin <vargas11742@gmail.com>
2022-03-07 18:22:42 -0500

    pintar cartas

--- js/script.js
+++ js/script.js
@@ -2,11 +2,33 @@ const getAll = async() => {
     try {
         const data = await fetch(`https://server-games-app.herokuapp.com/games`);
         const json = await data.json();
+        console.log(json)
+        pintarCartas(json)
         return json
     } catch (error) {
         console.log(error);
     }
 };
+getAll();
 
+let space = document.getElementById('cartas');
 
+async function pintarCartas(data) {
+    console.log(data)
+space.innerHTML = ''
+    data.forEach(element => {
+        const { name, price, genre, cover} = element;
+        space.innerHTML += `
+                <div class="card" style="width: 18rem;">
+            <div class="mosaico">
 
+                <img src="${cover}" class="card-img-top" alt="...">
+            </div>
+            <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <h6 class="card-title">${genre}</h6>
+            <p class="card-text fw-bold fs-3">${price}$<button class="btn btn-dark float-end" type="button" id="agregar">Comprar</button></p>
+            </div>
+        </div>`
+    });
+}
