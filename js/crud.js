54860a153dc94f95fe4a24e1200b9c69d3e14573
Sebastián <arevalosebastian081@gmail.com>
2022-03-07 20:33:32 -0500

    crud

--- js/crud.js
+++ js/crud.js
@@ -1,101 +1,136 @@
-const getAll = async() => {
-    try {
-        const data = await fetch(`https://server-games-app.herokuapp.com/games`);
-        const json = await data.json();
-        console.log(json)
-
-        return json
-    } catch (error) {
-        console.log(error);
-    }
+const getAll = async () => {
+  try {
+    const data = await fetch(`https://server-games-app.herokuapp.com/games`);
+    const json = await data.json();
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
 };
-getAll();
 
-let url = 'https://server-games-app.herokuapp.com/games/'
-
-let guardar = document.getElementById('btnGuardar');
-let btnNombre = document.getElementById('btnNombre')
-let btnEditar = document.getElementById('btnEditar')
-let btnEliminar = document.getElementById('btnEliminar')
+let btnGuardar = document.getElementById("btnGuardar");
+let btnNombre = document.getElementById("btnNombre");
+let btnEditar = document.getElementById("btnEditar");
+let btnEliminar = document.getElementById("btnEliminar");
 
 // crud
-guardar.addEventListener('click', (e) => {
-    e.preventDefault();
-    try {
-        fetch("https://server-games-app.herokuapp.com/games", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                name: document.querySelector('#name').value,
-                price: document.querySelector('#price').value,
-                description: document.querySelector('#description').value,
-                genre: document.querySelector('#genre').value,
-                cover: document.querySelector('#imagen').value,
-                platform: document.querySelector('#platform').value
-            }),
-          });
-          alert('juego guardado')
-    } catch (error) {
-        console.log(error);
-    }
-})
+btnGuardar.addEventListener("click", (e) => {
+  const fixString = (string) => {
+    return string.replace(/\w\S*/g, (w) =>
+      w.replace(/^\w/, (c) => c.toUpperCase())
+    );
+  };
+  e.preventDefault();
+  try {
+    fetch("https://server-games-app.herokuapp.com/games", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: fixString(document.querySelector("#name").value),
+        price: document.querySelector("#price").value,
+        description: document.querySelector("#description").value,
+        genre: document.querySelector("#genre").value,
+        cover: document.querySelector("#imagen").value,
+        platform: document.querySelector("#platform").value,
+      }),
+    });
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Juego guardado",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    document.querySelector("#name").value = ``;
+    document.querySelector("#price").value = ``;
+    document.querySelector("#description").value = ``;
+    document.querySelector("#genre").value = ``;
+    document.querySelector("#imagen").value = ``;
+    document.querySelector("#platform").value = ``;
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-btnNombre.addEventListener('click', async () => {
-    let name1 = document.getElementById('name').value;
+btnNombre.addEventListener("click", async () => {
+  let name1 = document.getElementById("name").value;
 
-    let resp = await fetch(url);
-    let data = await resp.json();
+  const fixString = (string) => {
+    return string.replace(/\w\S*/g, (w) =>
+      w.replace(/^\w/, (c) => c.toUpperCase())
+    );
+  };
 
-        // let modificar = data.find(user => user.correo === email);  cualquiera de los dos funciona
-        let modificar = data.find(user => user.name.includes(name1));
+  let resp = await getAll();
 
-        const {name, price,description, genre,cover, platform, id} = modificar;
+  // let modificar = data.find(user => user.correo === email);  cualquiera de los dos funciona
+  let modificar = resp.find((user) => user.name.includes(fixString(name1)));
 
-        document.getElementById('name').value = name;
-        document.getElementById('price').value = price;
-        document.getElementById('description').value = description;
-        document.getElementById('genre').value = genre;
-        document.getElementById('imagen').value = cover;
-        document.getElementById('platform').value = platform;
-        document.getElementById('id').value = id;
-        
-        console.log(modificar);
-})
+  const { name, price, description, genre, cover, platform, id } = modificar;
 
-btnEditar.addEventListener('click', async () => {
-    let nameModificar = document.getElementById('name').value;
-    let priceNameModificar = document.getElementById('price').value;
-    let descriptionModificar = document.getElementById('description').value;
-    let genreModificar = document.getElementById('genre').value;
-    let imagenModificar = document.getElementById('imagen').value;
-    let platformModificar = document.getElementById('platform').value;
-    let idModificar = document.getElementById('id').value;
+  document.querySelector("#name").value = name;
+  document.querySelector("#price").value = price;
+  document.querySelector("#description").value = description;
+  document.querySelector("#genre").value = genre;
+  document.querySelector("#imagen").value = cover;
+  document.querySelector("#platform").value = platform;
+  document.querySelector("#id").value = id;
 
-    await fetch(url+idModificar,{
-        method: 'PUT',
-        body: JSON.stringify({
-            name: nameModificar,
-            price: priceNameModificar,
-            description: descriptionModificar,
-            genre: genreModificar,
-            imagen: imagenModificar,
-            platform: platformModificar,
-            id: idModificar
-        }),
-        headers: {
-            "Content-Type": "application/json; charset=UTF-8"
-        }
-    })
-})
+});
 
-btnEliminar.addEventListener('click', async () => {
-    let idEliminar = document.getElementById('id').value;
-    await fetch(url + idEliminar,{
-        method: 'DELETE'
-    })
-    // alert("juego borrado")
+btnEditar.addEventListener("click", async () => {
+
+  let idModificar = document.querySelector("#id").value;
+
+  await fetch(`https://server-games-app.herokuapp.com/games/${idModificar}`, {
+    method: "PUT",
+    body: JSON.stringify({
+        name: document.querySelector("#name").value,
+        price: document.querySelector("#price").value,
+        description: document.querySelector("#description").value,
+        genre: document.querySelector("#genre").value,
+        cover: document.querySelector("#imagen").value,
+        platform: document.querySelector("#platform").value,
+    }),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  });
+  Swal.fire({
+    position: "center",
+    icon: "success",
+    title: "Juego editado",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+  document.querySelector("#name").value = ``;
+  document.querySelector("#price").value = ``;
+  document.querySelector("#description").value = ``;
+  document.querySelector("#genre").value = ``;
+  document.querySelector("#imagen").value = ``;
+  document.querySelector("#platform").value = ``;
+});
+
+btnEliminar.addEventListener("click", async () => {
+  let idEliminar = document.querySelector("#id").value;
+  await fetch(`https://server-games-app.herokuapp.com/games/${idEliminar}`, {
+    method: "DELETE",
+  });
+  Swal.fire({
+    position: "center",
+    icon: "success",
+    title: "Juego eliminado",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+  document.querySelector("#name").value = ``;
+  document.querySelector("#price").value = ``;
+  document.querySelector("#description").value = ``;
+  document.querySelector("#genre").value = ``;
+  document.querySelector("#imagen").value = ``;
+  document.querySelector("#platform").value = ``;
 });
 
 // // pintar pintarCartas
